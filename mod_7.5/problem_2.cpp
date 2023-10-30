0d0f09cf9981bb5550c28bf3fe6b65f84480627f
Shorif Ahmad <shorif8195@gmail.com>
2023-10-30 23:16:55 +0600

    solved 2 problem

--- mod_7.5/problem_2.cpp
+++ mod_7.5/problem_2.cpp
@@ -1,42 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=1e3+5;
+
+const int N = 1e3+5;
 vector<int>adj[N];
-bool visited[N];
-vector<int>ans;
+
+int visited[N];
+int level[N];
 void bfs(int s){
     queue<int>q;
     q.push(s);
     visited[s]=true;
-    
+    level[s]=0;
+
     while(!q.empty()){
-        int u = q.front();
-        ans.push_back(u);
+        int n = q.front();
         q.pop();
-       for(int v:adj[u]){
-             if(visited[v])continue;
-        
+
+        for(int v:adj[n]){
+            if(visited[v])continue;
             q.push(v);
 
             visited[v]=true;
-       }
-    }
+            level[v]= level[n]+1;
 
-    
+
+        }
+
+    }
 }
 int main(){
     int n,m;cin>>n>>m;
-
     for(int i=1;i<=m;i++){
         int u,v;cin>>u>>v;
         adj[u].push_back(v);
         adj[v].push_back(u);
+
     }
     bfs(1);
-    
-    reverse(ans.begin(),ans.end());
-    for(int i=0;i<n;i++){
-        cout<<ans[i]<<" ";
-    }
+    int x;cin>>x;
+    cout<<"Level of "<<x<<" = "<<level[x];
+
     return 0;
 }
